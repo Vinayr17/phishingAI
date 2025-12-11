aba0bb287578709e63ea57571c382000d5df8cf8
Vinayr17 <vinaydiwan09@gmail.com>
2025-12-11 16:19:06 +0100

    Update config.py

--- config.py
+++ config.py
@@ -9,11 +9,12 @@ Beispiele:
 TRACKING_DISPLAY_TEXT: Der sichtbare Text des Links in der Email.
 """
 
-# TODO: Anpassen, sobald der Tracking-Server bereitsteht.
-# Für lokale Tests auf Handy (im gleichen WLAN): Nutze deine lokale IP
-# Für echtes Experiment: Später online deployen (Railway/Render)
-TRACKING_URL = "http://172.21.89.58:5000/track"  # Lokale IP für Tests
-# TRACKING_URL = "http://localhost:5000/track"  # Nur für PC-Tests
+# Tracking-Link (aktuell Bitly-Shortlink, leitet auf Render-Track-Endpoint weiter)
+# Wenn du den Shortlink ändern willst, passe nur diese Zeile an.
+TRACKING_URL = "https://bit.ly/4rWbnuc"
+# TRACKING_URL = "https://phishingai.onrender.com/track"  # Online auf Render
+# TRACKING_URL = "http://172.21.89.58:5000/track"         # Lokale IP für Tests (auskommentiert)
+# TRACKING_URL = "http://localhost:5000/track"            # Nur für PC-Tests (auskommentiert)
 
 # Sichtbarer Text in der Email für den Tracking-Link.
 TRACKING_DISPLAY_TEXT = "moodle.uni-hamburg.de/dokument"
